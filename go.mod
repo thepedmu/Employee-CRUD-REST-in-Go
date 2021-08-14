0cd9d6c3d933355da3243fbd2811aeacd92878c2
thepedmu <padmarajxoxo6@gmail.com>
2021-08-14 19:55:06 +0530

    Implemented basic CRUD APIs without using GORM

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/thepedmu/Employee-CRUD-REST-in-Go
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.10.2
+	github.com/rs/cors v1.8.0
+)
--- main.go
+++ main.go
@@ -8,84 +8,110 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/rs/cors"
-
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-type employee struct {
-	gorm.Model
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
+var schema = `CREATE SEQUENCE IF NOT EXISTS public.employees_id_seq
+INCREMENT 1
+START 1
+MINVALUE 1
+MAXVALUE 2147483647
+CACHE 1;
+CREATE TABLE IF NOT EXISTS employees
+ (
+	id integer NOT NULL DEFAULT nextval('employees_id_seq'::regclass),
+	 name text COLLATE pg_catalog."default",
+	 age integer,
+	 CONSTRAINT employees_pkey PRIMARY KEY (id)
+ )
+ `
 
-var db *gorm.DB
+type employee struct {
+	ID   int    `db:"id" json:"id,omitempty"`
+	Name string `db:"name" json:"name,omitempty"`
+	Age  int    `db:"age" json:"age,omitempty"`
+}
 
 var err error
 
-var (
-	emp = []employee{
-		{Name: "sasa", Age: 12},
-		{Name: "sa", Age: 21},
-	}
-)
-
-func GetEmployees(w http.ResponseWriter, r *http.Request) {
-	var emps []employee
-	db.Find(&emps)
-	json.NewEncoder(w).Encode(&emps)
-}
-
-func GetEmployee(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var emp employee
-	db.First(&emp, params["id"])
-	json.NewEncoder(w).Encode(&emp)
-}
-
-func CreateEmployee(w http.ResponseWriter, r *http.Request) {
-	var emp employee
-	_ = json.NewDecoder(r.Body).Decode(&emp)
-	db.Create(&emp)
-}
-
-func UpdateEmployee(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var emp employee
-	db.First(&emp, params["id"])
-	_ = json.NewDecoder(r.Body).Decode(&emp)
-	db.Save(&emp)
-}
-
-func DeleteEmployee(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var emp employee
-	db.Delete(&emp, params["id"])
-}
-
-func main() {
-	godotenv.Load()
+func Connect() *sqlx.DB {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	name := os.Getenv("DB_NAME")
 	ssl := os.Getenv("SSLMODE")
 	password := os.Getenv("DB_PASSWORD")
+	db := sqlx.MustOpen("postgres", fmt.Sprintf("host=%s port=%v user=%s dbname=%s sslmode=%s password=%s", host, port, user, name, ssl, password))
+	return db
+
+}
+func GetEmployees(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	var emps []employee
+	err = db.Select(&emps, "SELECT ID,Name,Age FROM employees")
+	if err != nil {
+		fmt.Fprintln(w, "Could not retreieve Employees")
+		return
+	}
+	json.NewEncoder(w).Encode(emps)
+}
+
+func GetEmployee(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	params := mux.Vars(r)
+	var emp employee
+	err = db.Get(&emp, "SELECT id,name,age from employees WHERE id=$1", params["id"])
+	if err != nil {
+		fmt.Fprintln(w, "Could not retreieve Employee")
+		return
+	}
+	json.NewEncoder(w).Encode(&emp)
+}
+
+func CreateEmployee(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	var emp employee
+	_ = json.NewDecoder(r.Body).Decode(&emp)
+	tx := db.MustBegin()
+	tx.MustExec("INSERT INTO employees (Name, Age) VALUES ($1, $2)", emp.Name, emp.Age)
+	tx.Commit()
+
+	fmt.Fprint(w, "Creation Successful")
+}
+
+func UpdateEmployee(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	params := mux.Vars(r)
+	var emp employee
+	_ = json.NewDecoder(r.Body).Decode(&emp)
+	tx := db.MustBegin()
+	tx.MustExec("UPDATE employees SET Name=$1, Age=$2 WHERE id=$3", emp.Name, emp.Age, params["id"])
+	tx.Commit()
+
+	fmt.Fprint(w, "Updation Successful")
+}
+
+func DeleteEmployee(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	params := mux.Vars(r)
+	tx := db.MustBegin()
+	tx.MustExec("DELETE FROM employees WHERE id=$1", params["id"])
+	tx.Commit()
+
+	fmt.Fprint(w, "Deletion Successful")
+}
+
+func main() {
+	godotenv.Load()
+
+	db := Connect()
+
 	myRouter := mux.NewRouter()
 
-	db, err = gorm.Open("postgres", fmt.Sprintf("host=%s port=%v user=%s dbname=%s sslmode=%s password=%s", host, port, user, name, ssl, password))
-	if err != nil {
-		panic("Database access Failed!")
-	}
-	defer db.Close()
-
-	db.AutoMigrate(&employee{})
-
-	for index := range emp {
-		db.Create(&emp[index])
-	}
+	db.MustExec(schema)
 
 	myRouter.HandleFunc("/employees", GetEmployees).Methods("GET")
 	myRouter.HandleFunc("/employees/{id}", GetEmployee).Methods("GET")
@@ -95,5 +121,6 @@ func main() {
 
 	handler := cors.Default().Handler(myRouter)
 
-	log.Fatal(http.ListenAndServe(":4000", handler))
+	fmt.Println("Server up...")
+	log.Fatal(http.ListenAndServe(":4001", handler))
 }
